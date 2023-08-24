7c101afed3218df756cb7b8faa5b0fa4ad015ae2
RpPrime <Nikitamatveev007@gmail.com>
2023-08-24 10:48:22 +0300

    Доп.задача готова

---
+++ Sem6.2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Сколько чисел отобразить?");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+
+int[] GetFib (int[] arr)
+{
+    arr[0] = 0;
+    arr[1] = 1;
+    for (int i = 2; i < arr.Length; i++)
+    {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    return arr;
+}
+
+GetFib(array);
+
+Console.WriteLine($"[{String.Join(",", array)}]");
