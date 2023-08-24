5cb2a73789333ca534735ec493a2a0b5d108ca42
RpPrime <Nikitamatveev007@gmail.com>
2023-08-24 10:31:44 +0300

    Задача 41 готова

---
+++ Sem6.1.1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Сколько будет чисел?");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"Колличетсво принимаемых цифр равно {array.Length}");
+    Console.WriteLine("Введите число:");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int result (int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
+
+ Console.WriteLine($"Колличетсво чисел больше 0 равно: {result(array)}");
+// Console.WriteLine($"{String.Join("", array)}");
